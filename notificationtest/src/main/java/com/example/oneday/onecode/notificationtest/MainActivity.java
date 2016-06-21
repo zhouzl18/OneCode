45dec8db4f9ba8d166a86684b14c536bd9a54be8
zhouzl18 <fenglianhai18@gmail.com>
2016-06-21 14:30:14 +0800

    Adding a module,which is called "NotificationTest".

--- notificationtest/src/main/java/com/example/oneday/onecode/notificationtest/MainActivity.java
+++ notificationtest/src/main/java/com/example/oneday/onecode/notificationtest/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.oneday.onecode.notificationtest;
 
+import android.annotation.TargetApi;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,7 +13,8 @@ import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
     private NotificationManager noticeManager;
-    private Notification notification;
+    private Notification.Builder builder;
+    private final int NOTICE_TESt = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         noticeManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notification = new Notification(R.mipmap.ic_launcher, "This is a notice ticker.", System.currentTimeMillis());
+        builder = new Notification.Builder(this);
+        builder.setSmallIcon(R.mipmap.home_more_white);
+        builder.setAutoCancel(true);
+        builder.setTicker("This is a ticker.");
+        builder.setContentIntent(getPendingIntent("This is a pending intent."));
+        builder.setContentTitle("This is a title");
+        builder.setContentText("This is a content.");
     }
 
+    PendingIntent getPendingIntent(String text){
+        return PendingIntent.getActivity(this, text.hashCode(),
+                new Intent(this, NoticeActivity.class), PendingIntent.FLAG_CANCEL_CURRENT);
+
+    }
+
+    @TargetApi(16)
     public void sendNotice(View view){
-        noticeManager.notify(1, notification);
+        noticeManager.notify(NOTICE_TESt, builder.build());
     }
 }
---
+++ notificationtest/src/main/java/com/example/oneday/onecode/notificationtest/NoticeActivity.java
@@ -0,0 +1,18 @@
+package com.example.oneday.onecode.notificationtest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by One Day on 2016/6/21.
+ */
+
+public class NoticeActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notice_layout);
+    }
+}
